40acd539725c664db9267df4b5e68c43497475ce
Alifian Zulfaani <inaafilakoko27@gmail.com>
2025-02-12 21:52:20 +0700

    fix: position of submenu post in navbar large

--- resources/views/components/navbar-large.blade.php
+++ resources/views/components/navbar-large.blade.php
@@ -2,28 +2,32 @@
     class="fixed top-28 text-lg z-50 bg-primary w-full h-fit flex flex-col border-t-2 border-secondary">
     <!-- Menu Utama -->
     <div class="flex flex-row overflow-x-auto w-full h-12 border-b border-yellow-600 px-14 gap-10">
-        <a data-menu id="ikasda" href="#" class="flex items-center text-black p-4">
+        <a data-menu id="tentang-ikasda" href="#"
+            class="flex items-center text-black p-4 hover:text-white transition duration-300 ease-out">
             <div class="ml-2 text-left">
                 <h1 class="font-bold text-base">Tentang Ikasda</h1>
             </div>
         </a>
-        <a data-menu id="berita-ikasda" href="{{ route('posts.index') }}" class="flex items-center text-black p-4">
+        <a data-menu id="berita-ikasda" href="{{ route('posts.index') }}"
+            class="flex items-center text-black p-4 hover:text-white transition duration-300 ease-out">
             <div class="ml-2 text-left">
                 <h1 class="font-bold text-base">Berita Ikasda</h1>
             </div>
         </a>
-        <button data-menu id="layanan-alumni" class="cursor-pointer flex items-center text-black p-4">
+        <button data-menu id="layanan-alumni"
+            class="cursor-pointer flex items-center text-black p-4 hover:text-white transition duration-300 ease-out">
             <div class="ml-2 text-left">
                 <h1 class="font-bold text-base">Layanan Alumni</h1>
             </div>
         </button>
-        <button data-menu id="data-alumni" class="cursor-pointer flex items-center text-black p-4">
+        <button data-menu id="data-alumni"
+            class="cursor-pointer flex items-center text-black p-4 hover:text-white transition duration-300 ease-out">
             <div class="ml-2 text-left">
                 <h1 class="font-bold text-base">Data alumni</h1>
             </div>
         </button>
         <a data-menu id="market-alumni" href="{{ route('merchandise.index') }}"
-            class="flex items-center text-black p-4">
+            class="flex items-center text-black p-4 hover:text-white transition duration-300 ease-out">
             <div class="ml-2 text-left">
                 <p class="font-bold text-base">Market Alumni</p>
             </div>
@@ -31,13 +35,13 @@
     </div>
 
     <!-- Sub Menu -->
-    <div class="flex flex-row w-full h-auto px-14 text-black">
-        <div data-menu data-menu-target="ikasda" id="submenu-tentang-ikasda"
+    <div class="flex flex-row w-full h-auto px-12 text-black">
+        <div data-menu data-menu-target="tentang-ikasda" id="submenu-tentang-ikasda"
             class="hidden flex-col gap-2 overflow-x-auto w-full border-b border-white py-4 px-6">
             @foreach ($this->organizationals as $organizational)
                 @if ($organizational->slug !== 'contact' && $organizational->slug !== 'general')
                     <a wire:key="{{ $organizational->id }}" href="{{ route('abouts.show', $organizational->slug) }}"
-                        class="text-base hover:text-white p-3">
+                        class="text-base hover:text-white p-3 transition duration-300 ease-out">
                         {{ $organizational->title }}
                     </a>
                 @endif
@@ -45,14 +49,14 @@
         </div>
         <div data-menu data-menu-target="berita-ikasda"
             class="hidden flex-col gap-2 overflow-x-auto w-full border-b border-white py-4 px-6">
-            <div class="flex flex-row">
+            <div class="flex flex-col">
                 @foreach ($this->posts as $post)
                     <div id="{{ $post->slug }}" data-menu data-submenu="{{ $post->slug }}"
                         wire:key="{{ $post->id }}" class="flex items-center pr-2">
-                        <a href="{{ route('posts.detail', $post->slug) }}" class="text-base hover:text-white p-3">
+                        <a href="{{ route('posts.detail', $post->slug) }}"
+                            class="text-base hover:text-white p-3 transition duration-300 ease-out">
                             {{ $post->name }}
                         </a>
-                        <img src="/images/navbar/arrow.svg" alt="arrow" class="w-5 aspect-square">
                     </div>
                 @endforeach
             </div>
